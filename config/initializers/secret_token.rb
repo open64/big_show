a05ac89d4f2bcfa14da51c069fbec2b2d407a073
Yura Zhytskiy <systemsofdown@gmail.com>
2014-03-27 11:43:01 +0200

    empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BigShop::Application.config.secret_key_base = 'e2e399fe2756e9e8e1b4a3fcfd0775d629e9c26d5472348cc0e5c107e4ea1ca6f7575bc32b5f10de77bb1f0febaf15cb7e8af50bb66b4930d796a5c50b6ec1d2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BigShop::Application.config.session_store :cookie_store, key: '_big_shop_session'
